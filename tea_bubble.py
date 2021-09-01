83c5b8e48a7608a54f4992ce275b63d716bb283b
Wei Zeng <wzeng@ucsd.edu>
2021-08-31 23:24:39 -0600

    Random Spawning Functionality Completed

--- tea_bubble.py
+++ tea_bubble.py
@@ -29,20 +29,39 @@ class TeaBubble:
         self.refill_amount = refill_amount
         self.draw_index = 0
 
-    def generate_rand_tea_bub(self):
+    def rand_tea_bub_pos(self):
         """
-        generate_rand_tea_bub(self):
-
-        :return:    Update the position rect position with a randomly 2D coord. 800 <= X < 1600, 0 <= Y < 800
+        rand_tea_bub_pos(self):
+        :return:    Randomly update the position rect position with a randomly 2D coord. 800 <= X < 1600, 0 <= Y < 800
         """
         random.seed(pygame.time.get_ticks())
-        rand_x = random.randrange(800, 1599, 1)
+        rand_x = random.randrange(800 - self.position_rect.width, 1599 - self.position_rect.height, 1)
         rand_y = random.randrange(0, 899, 1)
 
         self.position_rect.x = rand_x
         self.position_rect.y = rand_y
 
         logging.info('Randomized Position at: ' + str(self.position_rect.x) + ', ' + str(self.position_rect.y))
+
+
+def gen_rand_tea_bubble_per_n_second(n: int, last_bubble_gen_time: list, bubble_image_path: str):
+    """
+    gen_rand_tea_bubble_per_n_second(n: int, last_bubble_gen_time: list, bubble_image_path: str):
+    :param n:                       Num of second to spawn a new tea bubble
+    :param last_bubble_gen_time:    List type for data modification, update to the time when new bubble got made
+    :param bubble_image_path:       Path to the tea bubble animation png directory
+    :return:                        TeaBubble Object being constructed or None
+    """
+    now = pygame.time.get_ticks()
+
+    # Every n Seconds
+    if now - last_bubble_gen_time[0] > n * 1000:
+        tea_bub = TeaBubble(800, 450, bubble_image_path, 20, 46, 51)
+        tea_bub.rand_tea_bub_pos()
+        last_bubble_gen_time[0] = now
+        return tea_bub
+    else:
+        return None
 
 
 def refill_tea(pot: Container, tea_bub: TeaBubble):
@@ -55,40 +74,65 @@ def refill_tea(pot: Container, tea_bub: TeaBubble):
     pot.tea_level += tea_bub.refill_amount
 
 
-def draw_bubble_animation(window, tea_bub: TeaBubble, last_update_bubble_time):
-    now = pygame.time.get_ticks()
-    if now - last_update_bubble_time > 30:
-        logging.info('Drawing bubble with image at index: ' + str(tea_bub.draw_index))
-        window.blit(tea_bub.image_list[tea_bub.draw_index].convert_alpha(),
-                    (tea_bub.position_rect.x, tea_bub.position_rect.y))
-        if tea_bub.draw_index == len(tea_bub.image_list) - 1:
-            tea_bub.draw_index = 0
-        else:
-            tea_bub.draw_index += 1
-    return now
+def draw_all_tea_bubble(window, tea_bubble_list: list):
+    """
+    draw_all_tea_bubble(window, tea_bubble_list: list):
+    :param window:              Window To Draw Bubble Onto
+    :param tea_bubble_list:     List With All Bubbles To Be Displayed
+    :return:                    Draw All Bubbles
+    """
+    for bub in tea_bubble_list:
+        draw_bubble_animation(window, bub)
+
+
+def draw_bubble_animation(window, tea_bub: TeaBubble):
+    """
+    draw_bubble_animation(window, tea_bub: TeaBubble, last_update_bubble_time):
+    :param window:      Window To Draw Bubble Onto
+    :param tea_bub:     Tea Bubble Object To Be Drawn
+    :return:            None
+    """
+    logging.info('Drawing bubble with image at index: ' + str(tea_bub.draw_index))
+    window.blit(tea_bub.image_list[tea_bub.draw_index].convert_alpha(),
+                (tea_bub.position_rect.x, tea_bub.position_rect.y))
+    if tea_bub.draw_index == len(tea_bub.image_list) - 1:
+        tea_bub.draw_index = 0
+    else:
+        tea_bub.draw_index += 1
 
 
 def test():
+    """
+    test():     Tester Function for Tea Bubble Functionalities
+    :return:    None
+    """
     # Initialize the game window
     window = pygame.display.set_mode((1600, 900))
     pygame.display.set_caption("Tea-Mates")
     image_path = os.path.join(ROOT_DIR, 'image/tea_bubble')
-    tea_bub = TeaBubble(800, 800, image_path, 20, 46, 51)
+    tea_bub = TeaBubble(800, 450, image_path, 20, 46, 51)
 
     run = True
     clock = pygame.time.Clock()
-    last_update_bubble_time = pygame.time.get_ticks()
+    game_start_time = pygame.time.get_ticks()
+    last_bubble_gen_time = [game_start_time]
+    all_bubble_list = []
 
     while run:
         clock.tick(30)
 
+        # Check For Window Close
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         # Setup White Background
         window.fill((255, 255, 255))
-        last_update_bubble_time = draw_bubble_animation(window, tea_bub, last_update_bubble_time)
+
+        new_bubble = gen_rand_tea_bubble_per_n_second(5, last_bubble_gen_time, image_path)
+        if new_bubble is not None:
+            all_bubble_list.append(new_bubble)
+        draw_all_tea_bubble(window, all_bubble_list)
         pygame.display.update()
 
 
