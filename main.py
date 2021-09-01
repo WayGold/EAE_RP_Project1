6ca25be9196cf86291ec3bbbba54331d1cdce671
Ogulcan Buyuksandalyaci <ogulcan.bs96@gmail.com>
2021-09-01 16:19:04 -0600

    Tea Bubbles Added to the game

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from delivery_zone import *
 from collision_container import *
 from barrier import *
 from barrier_parser import load_txt
+from tea_bubble import *
 import os
 import pygame
 import logging
@@ -85,7 +86,20 @@ def drop_tea(tea_drops, cup):
     return qualified
 
 
-def draw(window, map, obj_list, tea_drops, health, collected_tea):
+def float_tea_bubble(tea_bubbles, pot):
+    qualified = []
+
+    for bub in tea_bubbles:
+        if not tea_bubble_collision_detector(pot, bub):
+            qualified.append(bub)
+        else:
+            print('REFILLED')
+            refill_tea(pot, bub)
+
+    return qualified
+
+
+def draw(window, map, obj_list, tea_drops, health, collected_tea, tea_bubbles):
     """
     draw(window, obj_list, tea_drops):
     :param health:
@@ -112,7 +126,7 @@ def draw(window, map, obj_list, tea_drops, health, collected_tea):
                     (zone.get_global_position(map.starting_dx), 0))
 
         window.blit(zone.image,
-                    (zone.get_mirror_global_position(map.starting_dx), 0))           
+                    (zone.get_mirror_global_position(map.starting_dx), 0))
 
     # Draw Pot and Cup
     for i in obj_list:
@@ -120,9 +134,11 @@ def draw(window, map, obj_list, tea_drops, health, collected_tea):
 
     # Display Game Over image
     if map.are_delivery_zones_full():
-        window.blit(pygame.image.load(ROOT_DIR + r'/image/you_win.png'), (WIDTH / 3, HEIGHT / 3))
+        window.blit(pygame.image.load(
+            ROOT_DIR + r'/image/you_win.png'), (WIDTH / 3, HEIGHT / 3))
     elif health.life_count <= 0:
-         window.blit(pygame.image.load(ROOT_DIR + r'/image/game_over.png'), (WIDTH / 4, HEIGHT / 6))
+        window.blit(pygame.image.load(
+            ROOT_DIR + r'/image/game_over.png'), (WIDTH / 4, HEIGHT / 6))
 
     # Draw tea drops
     for d in tea_drops:
@@ -131,11 +147,16 @@ def draw(window, map, obj_list, tea_drops, health, collected_tea):
     # Draw Healthbar
     i = 0
     while i < health.life_count:
-        window.blit(health.image, (health.health_bar_pos_x + i * 60, health.health_bar_pos_y))
+        window.blit(health.image, (health.health_bar_pos_x +
+                    i * 60, health.health_bar_pos_y))
         i += 1
 
+    # Draw Tea Bubbles
+    draw_all_tea_bubble(window, tea_bubbles)
+
     # Init Collected Tea Display Text and Draw
-    textsurface = myfont.render('Tea Drops: ' + str(collected_tea), False, (0, 0, 0))
+    textsurface = myfont.render(
+        'Tea Drops: ' + str(collected_tea), False, (0, 0, 0))
     window.blit(textsurface, (1200, 70))
     window.blit(pygame.image.load(
         ROOT_DIR + r'/image/teadrop.png'), (1410, 70))
@@ -148,8 +169,7 @@ def is_game_on(life_count, map):
         return True
     else:
         return False
-    
-    
+
 
 def pot_control_listener(keys, pot):
     """
@@ -214,7 +234,7 @@ def main():
 
     for bNode in barriers_data:
         barriers.append(Barrier(bNode))
-    
+
     for bNode in delivery_zone_data:
         delivery_zones.append(DeliveryZone(bNode))
 
@@ -229,13 +249,17 @@ def main():
     health = Health(ROOT_DIR + r'/image/teacup.png')
 
     # Initialize game objects
-    game_map = Map(ROOT_DIR + r'/image/Background.png', barriers, delivery_zones)
+    game_map = Map(ROOT_DIR + r'/image/Background.png',
+                   barriers, delivery_zones)
     cup = Container(0, 600, ROOT_DIR + r'/image/teacup.png', 0, 101, 87)
     pot = Container(0, 100, ROOT_DIR + r'/image/teapot.png', 50, 143, 106)
 
     collected_tea = 0
 
     qualified_drops = []
+    tea_bub_image_path = os.path.join(ROOT_DIR, 'image/tea_bubble')
+    last_bubble_gen_time = [start_time]
+    all_bubble_list = []
     # Main Execution Loop
     while run:
         clock.tick(FPS)
@@ -245,6 +269,10 @@ def main():
 
         now = pygame.time.get_ticks()
         if is_game_on(health.life_count, game_map):
+            new_bubble = gen_rand_tea_bubble_per_n_second(
+                5, last_bubble_gen_time, tea_bub_image_path)
+            if new_bubble is not None:
+                all_bubble_list.append(new_bubble)
             for zone in delivery_zones:
                 zone.detect_collision(cup, game_map)
                 zone.detect_mirror_collision(cup, game_map)
@@ -255,15 +283,20 @@ def main():
                 last_tea_drop_time = now
             collected_tea = cup.tea_level
             qualified_drops = drop_tea(qualified_drops, cup)
+            all_bubble_list = float_tea_bubble(all_bubble_list, pot)
             keys_pressed = pygame.key.get_pressed()
             pot_control_listener(keys_pressed, pot)
             cup_control_listener(keys_pressed, cup)
+
+            for bub in all_bubble_list:
+                if tea_bubble_collision_detector(pot, bub):
+                    refill_tea(pot, bub)
 
             # Using the new can_receive_damage bosol of containers to give some breathing room after taking a damage
             # DAMAGE_RECEIVING_CD is set to 3000 milliseconds (3 seconds) Damage timer starts immediately after a
             # collision happens and counts to 3 and then can_receive_damage is set back to True
             if cup.can_receive_damage and pot.can_receive_damage:
-                draw(window, game_map, [pot, cup], qualified_drops, health, collected_tea)
+                draw(window, game_map, [pot, cup], qualified_drops, health, collected_tea, all_bubble_list)
                 if collision_detector(pot, cup):
                     damage_timer = now
                     health.life_count = 0
@@ -274,9 +307,11 @@ def main():
             else:
                 time_passed = now - damage_timer
                 if time_passed < 500 or (1000 < time_passed < 1500) or (2000 < time_passed < 2500):
-                    draw(window, game_map, [], qualified_drops, health, collected_tea)
+                    draw(window, game_map, [], qualified_drops,
+                         health, collected_tea, all_bubble_list)
                 else:
-                    draw(window, game_map, [pot, cup], qualified_drops, health, collected_tea)
+                    draw(window, game_map, [
+                         pot, cup], qualified_drops, health, collected_tea, all_bubble_list)
                 # Reset the damage timer 3 seconds after getting damaged
                 if now - damage_timer > DAMAGE_RECEIVING_CD:
                     pot.can_receive_damage = True
@@ -285,7 +320,8 @@ def main():
             # Update TeaDrop Position
             pot.tea_drop_position_update()
         else:
-            draw(window, game_map, [], [], health, collected_tea)
+            draw(window, game_map, [], [], health,
+                 collected_tea, all_bubble_list)
         game_map.slideMap()
 
     pygame.quit()
