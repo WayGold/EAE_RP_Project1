e293651fb6b79487ea7648594ddbeab7fddf888e
Ogulcan Buyuksandalyaci <ogulcan.bs96@gmail.com>
2021-09-01 14:16:04 -0600

    delivery zone looping as object

--- delivery_zone.py
+++ delivery_zone.py
@@ -17,9 +17,13 @@ class DeliveryZone:
         self.tea_level = 0
         self.is_full = False
         self.tea_requirement = random.randint(10, 15)
+        self.mirror_image_x_pos = 2400 + self.pos_x
 
     def get_global_position(self, map_pos_x):
-        return self.pos_x + map_pos_x  
+        return self.pos_x + map_pos_x 
+
+    def get_mirror_global_position(self, map_pos_x) :
+        return self.mirror_image_x_pos + map_pos_x
 
     def detect_collision(self, container: Container, map: Map):
         # if container is no where near the barrier horizontally, there is no collision so return true immediately to
@@ -33,9 +37,32 @@ class DeliveryZone:
         if container_top_pos_y <= self.tip_points[0] or container_bottom_pos_y >= self.tip_points[1]:
             logging.info('Inside delivery zone')
             if container.tea_level > self.tea_requirement - self.tea_level:
+                container.tea_level -= self.tea_requirement - self.tea_level
                 self.tea_level = self.tea_requirement
                 self.is_full = True
+            else:
+                self.tea_level += container.tea_level
+                container.tea_level = 0
+            
+            return True
+        else:
+            return False
+
+    def detect_mirror_collision(self, container: Container, map: Map):
+        # if container is no where near the barrier horizontally, there is no collision so return true immediately to
+        # save power
+        if container.position_rect.x + container.position_rect.width < self.get_mirror_global_position(map.starting_dx) or container.position_rect.x > self.get_mirror_global_position(map.starting_dx) + self.width:
+            return False
+            
+        # get container top and bottom y coordinates
+        container_top_pos_y = container.position_rect.y
+        container_bottom_pos_y = container.position_rect.y + container.position_rect.height
+        if container_top_pos_y <= self.tip_points[0] or container_bottom_pos_y >= self.tip_points[1]:
+            logging.info('Inside delivery zone')
+            if container.tea_level > self.tea_requirement - self.tea_level:
                 container.tea_level -= self.tea_requirement - self.tea_level
+                self.tea_level = self.tea_requirement
+                self.is_full = True
             else:
                 self.tea_level += container.tea_level
                 container.tea_level = 0
